cc9f981265d423ba464b1ddc707dba2e6c3646ae
DiptamoySinha <diptamoysinha2021@outlook.com>
2025-01-20 21:39:45 +0530

    updated extent report manager

--- src/test/java/com/api/extentReportManager/ExtentReportManager.java
+++ src/test/java/com/api/extentReportManager/ExtentReportManager.java
@@ -3,17 +3,31 @@ package com.api.extentReportManager;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
+import java.io.File;
+
 public class ExtentReportManager {
 
     public static ExtentReports extentReport;
 
     public static ExtentReports createInstance() {
-        extentReport = new ExtentReports();
-        String reportFilePath = System.getProperty("user.dir") + "\\reports\\" + "extent.html";
+        ExtentReports extentReport = new ExtentReports();
+
+        // Define the report directory and ensure it exists
+        String reportDirectory = System.getProperty("user.dir") + File.separator + "reports";
+        File reportsDir = new File(reportDirectory);
+        if (!reportsDir.exists()) {
+            reportsDir.mkdirs();
+        }
+
+        // Define the report file path
+        String reportFilePath = reportDirectory + File.separator + "extent.html";
+
+        // Set up ExtentSparkReporter
         ExtentSparkReporter reporter = new ExtentSparkReporter(reportFilePath);
         extentReport.attachReporter(reporter);
         extentReport.setSystemInfo("OS", System.getProperty("os.name"));
-        extentReport.setSystemInfo("OS", System.getProperty("java.version"));
+        extentReport.setSystemInfo("Java Version", System.getProperty("java.version"));
+
         return extentReport;
     }
 }
